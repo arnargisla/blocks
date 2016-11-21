bb4a0c4c2f9932f6e49d3ce51c1b49a9df873445
Arnar Þór <arnargisla@gmail.com>
2016-11-21 20:39:40 +0000

    Remove unused files

--- src/server/sendPlayerUpdates.js
+++
@@ -1,7 +0,0 @@
-function sendPlayerUpdates(io, players){
-  const message = { players: [] };
-  players.forEach((player, id)=>message.players.push(player));
-  io.emit('players update', message);
-}
-
-module.exports = sendPlayerUpdates;
